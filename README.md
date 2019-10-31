2ea9d9116f70c98166a908807a6dea293eac10b9
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-31 14:44:10 -0700

    clean canvas

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Clicking on the deactivation buttons positions the button somewhere else. This piece conveys the way design is used to trick users into doing something they intentionally don't mean to do. Or, make it harder for the user to do something. For example, one trying to deactivate an account and having to go through many links and pages to find out on how to deactivate the account. 
+p5 sound
--- sketch.js
+++ sketch.js
@@ -1,32 +1,8 @@
-var button;
-var x=500;
-var y=500;
-var nabutton;
 
 
 function setup() {
-	// make canvas full screen
-	createCanvas(windowWidth, windowHeight);
-	// button=createButton('deactivate your account');
-	button = select('#dabutton');
-	button.style("background-color","#F6F6F6");
-	button.style("padding","2px");
-	button.position(x,y);
-	button.mousePressed(randomLocation);
-
-	nabutton = select('#nabutton');
-	nabutton.style("background-color","#3D87F9");
-	nabutton.style("padding","15px");
-	nabutton.position(center);
+	
 }
-
-
-function randomLocation() {
-	 x=random(windowWidth)
-	 y=random(windowHeight)
-	 button.position(x,y);
-}
-
 
 
 
@@ -36,14 +12,6 @@ function randomLocation() {
 
 
 function draw() {
-	noStroke();
-	fill(255);
-	textAlign(CENTER);
-	text("ioegnoingeioseginsigenpseingopsegpnseopgnosengopegsegsgsegseg", 400, 500);
-
-
-
-
 
 
 
