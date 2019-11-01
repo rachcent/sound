c958e37b46d7efc28a712b659d37d80c6af814f8
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-31 17:10:13 -0700

    f

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 
+let dolphinImage;
+let vaporButton;
+let ecoSong;
+
+function preload() {
+	soundFormats('mp3','ogg');
+	ecoSong = loadSound('assets/ecozones.mp3');
+}
+vaporButton.mousePressed(toggleDolphin);
 
 function setup() {
-	
+ noCanvas();
+ vaporButton = select('#vaporButton');
+ dolphinImage = select('#dolphin');
+
+function toggleDolphin(){
+	ecoSong.setVolume(0.3);
+	ecoSong.play();
+
+}
+
+
+
 }
 
 
